95c6fc4831b6e8e16a3d2a953445207ee4df5de4
A <118105047+Aguerrow@users.noreply.github.com>
2023-02-18 09:51:07 +0800

    Add files via upload
    uploaded the files

---
+++ main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> TAXXY </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./assets/css/design.css">
+
+</head>
+<body>
+    <div class="banner">
+        <div class="content">
+            <h1>TAXXY: A TAX CALCULATOR WEB APP</h1>
+            <p>Welcome to TAXXY, feel free to use this website to calculate your taxes!</p>
+        </div>
+        <div class="container">
+
+            <!-- <button class="btn1" type="button">COMPUTE</button> -->
+            <?php include("./assets/calcu.php"); ?>
+        </div>
+    </div>
+</body>
+</html>
