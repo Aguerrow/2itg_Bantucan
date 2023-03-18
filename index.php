bd583add6a3af118c74d29159bbbce9e979aa19b
Adrian Bantucan <adrian.bantucan.cics@ust.edu.ph>
2023-03-18 20:12:54 +0800

    added the next step and modified the file

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
-
+// Parent Class
 class Employee
 {
     private $name;
     private $salary;
-
-
+    
     public function setName($name) 
     {
         $this->name = $name;
     }
-
-    public function getName()
+    
+    public function getName() 
     {
         return $this->name;
     }
-
+    
     public function setSalary($salary) 
     {
         $this->salary = $salary;
@@ -26,44 +25,116 @@ class Employee
     {
         return $this->salary;
     }
-
+    
     public function displayInfo() 
     {
-        echo "Employee's Basic Information: <br><br>";
-        echo "Name: " . $this->name . "<br>";
-        echo "Salary: " . $this->salary . "<br>";
+        echo "<br>Employee's Information<br>";
+        echo "<br>Name: " . $this->name . "<br>";
+        echo "Salary: " . "₱" . $this->salary . "<br>";
     }
-
 }
 
+
+// Child Class
 class Manager extends Employee 
 {
     private $department;
     
-    public function __construct($department) {
+    public function __construct($name, $salary, $department) 
+    {
+        $this->setName($name);
+        $this->setSalary($salary);
         $this->department = $department;
     }
     
-    public function getDepartment() {
+    public function getDepartment() 
+    {
         return $this->department;
     }
     
-    public function printInfo() {
+    public function printInfo() 
+    {
         parent::displayInfo();
-        echo "Department: " . $this->department;
+        echo "Department: " . $this->department . "<br>";
     }
 }
 
-$myManager = new manager("Accounting");
-$myManager->setName("Isagi Yoichi");
-$myManager->setSalary(25000);
+class Director extends Manager 
+{
+    private $carAllowance;
+    
+    public function __construct($name, $salary, $department, $carallowance) 
+    {
+        parent::__construct($name, $salary, $department);
+        $this->carallowance = $carallowance;
+    }
+    
+    public function getCarAllowance() 
+    {
+        return $this->carallowance;
+    }
+    
+    public function printInfo() 
+    {
+        parent::printInfo();
+        echo "Car Allowance: " . "₱" . $this->carallowance . "<br>";
+    }
+}
 
+class Executive extends Employee 
+{
+    private $position;
+    
+    public function __construct($name, $salary, $position) 
+    {
+        $this->setName($name);
+        $this->setSalary($salary);
+        $this->position = $position;
+    }
+    
+    public function getPosition() 
+    {
+        return $this->position;
+    }
+    
+    public function printInfo() 
+    {
+        parent::displayInfo();
+        echo "Position: " . $this->position . "<br>";
+    }
+}
 
-$name = $myManager->getName();
-$salary = $myManager->getSalary();
-$department = $myManager->getDepartment();
+class IT extends Executive 
+{
+    public function __construct($name, $salary) 
+    {
+        parent::__construct($name, $salary, "IT Head");
+    }
+}
 
+class Intern extends Executive 
+{
+    public function __construct($name, $salary) 
+    {
+        parent::__construct($name, $salary, "Intern");
+    }
+}
 
-$myManager->printInfo();
+// Hierarchical Inheritance
+$manager1 = new Manager("Seishiro Nagi", 25000, "Accounting");
+$manager1->printInfo();
+
+$executive1 = new Executive(" Meguro Bachira", 50000, "Secretary");
+$executive1->printInfo();
+
+// Multilevel Inheritance
+$director = new Director("Isagi Yoichi", 150000, "Admin", 10000);
+$director->printInfo();
+
+$it1 = new IT("Rensuke Kunigami", 90000);
+$it1->printInfo();
+
+$intern1 = new Intern("Hyoma Chigiri", 1000);
+$intern1->printInfo();
 
 ?>
